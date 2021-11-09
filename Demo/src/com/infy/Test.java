f95927081a50c36f1f132427d0c1eb58e2d3ad20
srikanth <srsforjava@gmail.com>
2021-11-09 18:06:47 +0530

    added new project

---
+++ Demo/src/com/infy/Test.java
@@ -0,0 +1,10 @@
+package com.infy;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is testing my project");
+	}
+
+}
